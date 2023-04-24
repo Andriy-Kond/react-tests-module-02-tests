5bcf3b053a809494e85252ea994410ac06e8be1b
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-04-24 17:02:17 +0300

    --

--- src/components/Button/Buttno.jsx
+++ src/components/Button/Buttno.jsx
@@ -1,3 +1,8 @@
-export const Button = ({ text, clickHandler }) => {
-  return <button onClick={clickHandler}>{text}</button>;
-};
+// export const Button = ({ text, clickHandler }) => {
+//   return <button onClick={clickHandler}>{text}</button>;
+// };
+let a = Math.floor(Math.random() * 16777215)
+  .toString(16)
+  .padStart(6, 0);
+
+console.log('getColor >> getColor:', a);
--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
